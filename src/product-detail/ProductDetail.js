2cbe757cbfec3970a21ad3c71b8cfba49385f84b
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-08-27 16:09:53 -0300

    added button to buy

--- src/product-detail/ProductDetail.js
+++ src/product-detail/ProductDetail.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import products from '../mock-data/products';
-import { API_KEY, ENCRIPT_KEY } from '../config';
+import {API_KEY, ENCRIPT_KEY} from '../config';
 import * as pagarme from 'pagarme';
 
 export default class ProductDetail extends Component {
@@ -99,79 +99,6 @@ export default class ProductDetail extends Component {
         }
       ]
     };
-    // const response = await axios.post('https://api.pagar.me/1/transactions', body);
-    // console.log(response);
-    // const client = await pagarme.client.connect({api_key: API_KEY});
-    // console.log(client);
-    // const transaction = await client.transaction.create(body);
-    // console.log(transaction);
-    pagarme.client.connect({ api_key: API_KEY })
-  .then(client => client.transactions.create({
-    "amount": 100,
-    "card_number": "5580400514542835",
-    "card_cvv": "124",
-    "card_expiration_date": "0819",
-    "card_holder_name": "João",
-    "customer": {
-      "external_id": "#3311",
-      "name": "João",
-      "type": "individual",
-      "country": "br",
-      "email": "joao_teste@hotmail.com",
-      "documents": [
-        {
-          "type": "cpf",
-          "number": "36945222047"
-        }
-      ],
-      "phone_numbers": ["+5513981456345"],
-      "birthday": "1996-10-25"
-    },
-    "billing": {
-      "name": "Trinity Moss",
-      "address": {
-        "country": "br",
-        "state": "sp",
-        "city": "Cotia",
-        "neighborhood": "Rio Cotia",
-        "street": "Rua Matrix",
-        "street_number": "9999",
-        "zipcode": "06714360"
-      }
-    },
-    "shipping": {
-      "name": "Neo Reeves",
-      "fee": 1000,
-      "delivery_date": "2000-12-21",
-      "expedited": true,
-      "address": {
-        "country": "br",
-        "state": "sp",
-        "city": "Cotia",
-        "neighborhood": "Rio Cotia",
-        "street": "Rua Matrix",
-        "street_number": "9999",
-        "zipcode": "06714360"
-      }
-    },
-    "items": [
-      {
-        "id": "r123",
-        "title": "Red pill",
-        "unit_price": 50,
-        "quantity": 1,
-        "tangible": true
-      },
-      {
-        "id": "b123",
-        "title": "Blue pill",
-        "unit_price": 50,
-        "quantity": 1,
-        "tangible": true
-      }
-    ]
-  }))
-  .then(transaction => console.log(transaction))
   }
 
   render() {
@@ -186,10 +113,87 @@ export default class ProductDetail extends Component {
             </div>
             <div className="column">
               <p className="title is-2">Detalhes do produto</p>
+              <p>Nome: {this.state.product.name}</p>
+              <p>Valor: {this.state.product.value}</p>
+              <p>Descrição: {this.state.product.description}</p>
+              <p>Cores: {this.state.product.colors}</p>
+              <p>Frete: {this.state.product.freight}</p>
             </div>
           </div>
+          <button onClick={() => this.buyProduct(this.state.product)}>Comprar</button>
         </div>
       </div>
     )
+  }
+
+  buyProduct(product) {
+    pagarme
+      .client
+      .connect({api_key: API_KEY})
+      .then(client => client.transactions.create({
+        "amount": 100,
+        "card_number": "5580400514542835",
+        "card_cvv": "124",
+        "card_expiration_date": "0819",
+        "card_holder_name": "João",
+        "customer": {
+          "external_id": "#3311",
+          "name": "João",
+          "type": "individual",
+          "country": "br",
+          "email": "joao_teste@hotmail.com",
+          "documents": [
+            {
+              "type": "cpf",
+              "number": "36945222047"
+            }
+          ],
+          "phone_numbers": ["+5513981456345"],
+          "birthday": "1996-10-25"
+        },
+        "billing": {
+          "name": "Trinity Moss",
+          "address": {
+            "country": "br",
+            "state": "sp",
+            "city": "Cotia",
+            "neighborhood": "Rio Cotia",
+            "street": "Rua Matrix",
+            "street_number": "9999",
+            "zipcode": "06714360"
+          }
+        },
+        "shipping": {
+          "name": "Neo Reeves",
+          "fee": 1000,
+          "delivery_date": "2000-12-21",
+          "expedited": true,
+          "address": {
+            "country": "br",
+            "state": "sp",
+            "city": "Cotia",
+            "neighborhood": "Rio Cotia",
+            "street": "Rua Matrix",
+            "street_number": "9999",
+            "zipcode": "06714360"
+          }
+        },
+        "items": [
+          {
+            "id": "r123",
+            "title": "Red pill",
+            "unit_price": 50,
+            "quantity": 1,
+            "tangible": true
+          }, {
+            "id": "b123",
+            "title": "Blue pill",
+            "unit_price": 50,
+            "quantity": 1,
+            "tangible": true
+          }
+        ]
+      }))
+      .then(transaction => console.log(transaction));
   }
 }
